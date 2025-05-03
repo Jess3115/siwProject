28bcab5cd7d1a38b40f3521aba509caa8641e0a7
Digias <86567479+Digias@users.noreply.github.com>
2025-05-03 18:46:07 +0200

    RecipeCategory

--- siwProject/src/main/java/it/uniroma3/siw/model/Recipe.java
+++ siwProject/src/main/java/it/uniroma3/siw/model/Recipe.java
@@ -1,18 +1,24 @@
 package it.uniroma3.siw.model;
 
+import java.util.List;
+
 import jakarta.persistence.*;
+
 
 @Entity
 public class Recipe {
 	@Id
 	@GeneratedValue (strategy = GenerationType.AUTO)
 	private Long id;
-	
+
 	private String name;
-	
+
 	@ManyToOne
 	private User creator;
 	
-	
-	
+	@ManyToMany(fetch = FetchType.EAGER, mappedBy = "recipes")
+	private List<RecipeCategory> category;
+
+
+
 }
--- siwProject/src/main/java/it/uniroma3/siw/model/RecipeCategory.java
+++ siwProject/src/main/java/it/uniroma3/siw/model/RecipeCategory.java
@@ -1,11 +1,69 @@
 package it.uniroma3.siw.model;
 
+import java.util.List;
+import java.util.Objects;
+
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 public class RecipeCategory {
 	@Id
-	@GeneratedValue (strategy = GenerationType.AUTO)
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@NotNull
+	private String name;
+	
+	@ManyToMany(fetch = FetchType.EAGER, mappedBy = "category")
+	private List<Recipe> recipes;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Recipe> getRecipes() {
+		return recipes;
+	}
+
+	public void setRecipes(List<Recipe> recipes) {
+		this.recipes = recipes;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name, recipes);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		RecipeCategory other = (RecipeCategory) obj;
+		return Objects.equals(id, other.id) && Objects.equals(name, other.name)
+				&& Objects.equals(recipes, other.recipes);
+	}
+
+	@Override
+	public String toString() {
+		return "RecipeCategory [id=" + id + ", name=" + name + ", recipes=" + recipes + "]";
+	}
+	
+	
 }
