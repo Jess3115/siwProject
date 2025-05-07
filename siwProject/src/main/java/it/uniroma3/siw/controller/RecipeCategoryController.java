054845a04ee5b7589ea82afb808f50ec9faf6fbe
Digias <86567479+Digias@users.noreply.github.com>
2025-05-07 12:26:48 +0200

    aggiunto mapping alla singola categoria

--- siwProject/src/main/java/it/uniroma3/siw/controller/RecipeCategoryController.java
+++ siwProject/src/main/java/it/uniroma3/siw/controller/RecipeCategoryController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import it.uniroma3.siw.model.RecipeCategory;
 import it.uniroma3.siw.service.RecipeCategoryService;
 
 @Controller
@@ -16,5 +18,13 @@ public class RecipeCategoryController {
 	public String showCategories(Model model) {
 		model.addAttribute("categories", this.recipeCategoryService.getAllCategories());
 		return "categories.html";
+	}
+	
+	@GetMapping("/category/{categoryID}")
+	public String getCategory(@PathVariable("categoryID") Long categoryID, Model model) {
+		RecipeCategory category = this.recipeCategoryService.getCategoryById(categoryID);
+		model.addAttribute("category", category);
+		model.addAttribute("recipes", category.getRecipes());
+		return "category.html";
 	}
 }
