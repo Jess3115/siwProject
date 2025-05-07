a802d1383cdf001c0ddb5b2716255884ce4c4d9b
Digias <86567479+Digias@users.noreply.github.com>
2025-05-07 11:21:38 +0200

    aggiunti metodi get e setter in grading

--- siwProject/src/main/java/it/uniroma3/siw/model/Grading.java
+++ siwProject/src/main/java/it/uniroma3/siw/model/Grading.java
@@ -1,6 +1,7 @@
 package it.uniroma3.siw.model;
 
 import java.util.List;
+import java.util.Objects;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
@@ -17,4 +18,52 @@ public class Grading {
 	
 	@OneToMany(mappedBy = "grading")
 	private List<Recipe> recipes;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	public List<Recipe> getRecipes() {
+		return recipes;
+	}
+
+	public void setRecipes(List<Recipe> recipes) {
+		this.recipes = recipes;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, recipes, value);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Grading other = (Grading) obj;
+		return Objects.equals(id, other.id) && Objects.equals(recipes, other.recipes) && value == other.value;
+	}
+
+	@Override
+	public String toString() {
+		return "Grading [id=" + id + ", value=" + value + ", recipes=" + recipes + "]";
+	}
+	
+	
 }
