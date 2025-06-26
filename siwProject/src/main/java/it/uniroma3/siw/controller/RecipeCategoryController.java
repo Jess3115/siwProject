3ec557b6b16110d4cd8d43eca7b3cf134ad4bfc5
Digias <86567479+Digias@users.noreply.github.com>
2025-06-26 11:34:49 +0200

    refactor: remove unused import for RecipeCategory in RecipeCategoryController

--- siwProject/src/main/java/it/uniroma3/siw/controller/RecipeCategoryController.java
+++ siwProject/src/main/java/it/uniroma3/siw/controller/RecipeCategoryController.java
@@ -6,7 +6,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import it.uniroma3.siw.model.RecipeCategory;
 import it.uniroma3.siw.service.RecipeCategoryService;
 
 @Controller
