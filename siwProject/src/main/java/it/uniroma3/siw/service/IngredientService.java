c4dd2ec7cd1d1a895e5bf8adc183400324a94e91
Digias <86567479+Digias@users.noreply.github.com>
2025-06-28 19:41:41 +0200

    feat: add ingredient management functionality with CRUD operations and views for ingredient listing and details

--- siwProject/src/main/java/it/uniroma3/siw/service/IngredientService.java
+++ siwProject/src/main/java/it/uniroma3/siw/service/IngredientService.java
@@ -3,6 +3,8 @@ package it.uniroma3.siw.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import it.uniroma3.siw.model.Ingredient;
+import it.uniroma3.siw.model.Recipe;
 import it.uniroma3.siw.repository.IngredientRepository;
 import jakarta.transaction.Transactional;
 
@@ -11,4 +13,24 @@ import jakarta.transaction.Transactional;
 public class IngredientService {
 
 	@Autowired IngredientRepository ingredientRepository;
+
+    public Iterable<Ingredient> getAllIngredients() {
+        return this.ingredientRepository.findAll();
+    }
+
+    public Iterable<Recipe> findRecipesByIngredientId(Long ingredientID) {
+        return this.ingredientRepository.findRecipesByIngredientId(ingredientID);
+    }
+
+    public Ingredient getIngredientById(Long ingredientID) {
+        return this.ingredientRepository.findById(ingredientID).orElse(null);
+    }
+
+    public void saveIngredient(Ingredient newIngredient) {
+        this.ingredientRepository.save(newIngredient);
+    }
+
+    public void deleteIngredientById(Long ingredientID) {
+        this.ingredientRepository.deleteById(ingredientID);
+    }
 }
