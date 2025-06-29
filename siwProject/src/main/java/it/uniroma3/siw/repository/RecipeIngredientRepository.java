6b0f822824c1bc0381132e1beb8151ba73c0ab01
Digias <86567479+Digias@users.noreply.github.com>
2025-06-29 16:22:13 +0200

    feat: add RecipeIngredientRepository and RecipeIngredientService for managing recipe ingredients

---
+++ siwProject/src/main/java/it/uniroma3/siw/repository/RecipeIngredientRepository.java
@@ -0,0 +1,8 @@
+package it.uniroma3.siw.repository;
+
+import it.uniroma3.siw.model.RecipeIngredient;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RecipeIngredientRepository extends CrudRepository<RecipeIngredient, Long>{
+
+}
---
+++ siwProject/src/main/java/it/uniroma3/siw/service/RecipeIngredientService.java
@@ -0,0 +1,31 @@
+package it.uniroma3.siw.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import it.uniroma3.siw.model.RecipeIngredient;
+import it.uniroma3.siw.repository.RecipeIngredientRepository;
+import jakarta.transaction.Transactional;
+
+@Service
+@Transactional
+public class RecipeIngredientService {
+    @Autowired
+    private RecipeIngredientRepository recipeIngredientRepository;
+
+    public Iterable<RecipeIngredient> getAllRecipeIngredients() {
+        return this.recipeIngredientRepository.findAll();
+    }
+
+    public RecipeIngredient getRecipeIngredientById(Long id) {
+        return this.recipeIngredientRepository.findById(id).orElse(null);
+    }
+
+    public void save(RecipeIngredient recipeIngredient) {
+        this.recipeIngredientRepository.save(recipeIngredient);
+    }
+
+    public void deleteById(Long id) {
+        this.recipeIngredientRepository.deleteById(id);
+    }
+}
