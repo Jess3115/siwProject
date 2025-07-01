b6f927665e881d2100628c04f92c787e4f392edc
Digias <francescodigianvincenzo@gmail.com>
2025-07-01 16:09:51 +0200

    Update background assets and improve section styling
    Moved and added new background images to a dedicated folder, updated AuthConfiguration to allow access to the new background path, and enhanced section styling in index.css with a blurred background and overlay for better readability. Also adjusted section title color in main-style.css for improved visual consistency.

--- siwProject/src/main/java/it/uniroma3/siw/authentication/AuthConfiguration.java
+++ siwProject/src/main/java/it/uniroma3/siw/authentication/AuthConfiguration.java
@@ -56,6 +56,7 @@ public class AuthConfiguration {
                                                                 "/",
                                                                 "/images/**",
                                                                 "/index",
+                                                                "/background/**",
                                                                 "/recipe",
                                                                 "/recipe/**",
                                                                 "/category/**",
